6f467389c764e9bb4877806e4e7117b3c7301dee
mahamatoumaroumate <mhtoumate1999@gmail.com>
2025-05-01 14:39:56 +0300

    update user

--- utils/jwt.js
+++ utils/jwt.js
@@ -14,7 +14,7 @@ const createUserToken=({user})=>{
 
  const attachCookiesToResponse=({res,user})=>{
     const token=createJWT({payload:user})
-    const oneDay=1000*60*60*24
+    const oneDay=1000 * 60 * 60 * 24
     res.cookie('token',token,{
         httpOnly:true,
         expires:new Date(Date.now()+oneDay),
