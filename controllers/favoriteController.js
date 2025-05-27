103a3446ebc9f8ada38a7de30df0582877aa721a
mahamatoumaroumate <mhtoumate1999@gmail.com>
2025-05-27 19:25:15 +0300

    update Favorite Controller

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,32 +1,48 @@
-const Favorite=require('../models/Favorite')
-const Product=require('../models/Product')
-const CustomErrors=require('../errors')
-const {StatusCodes}=require('http-status-codes')
-const addFavorite=async(req,res)=>{
-    const {user,product}=req.body
-    if(!user || !product){
-        throw new CustomErrors.BadRequest('please provide all the values user and product')
-    }
-    const dbProduct=await Product.find({_id:product})
-    if(!dbProduct)throw new CustomErrors.NotFound(`Sorry no product found with id : ${product}`)
-    const favorite=await Favorite.create({user,product})
-    res.status(StatusCodes.OK).json({favorite,msg:'successfully added favorite'})
-}
-const deleteFavorite=async(req,res)=>{
-    const {user,product}=req.query
-    if(!user || !product){
-        throw new CustomErrors.BadRequest('please provide all the values user and product')
-    }
-    const dbProduct=await Product.find({_id:product})
-    if(!dbProduct)throw new CustomErrors.NotFound(`Sorry no product found with id : ${product}`)
-    const favorite=await Favorite.findOneAndDelete({_id:req.params.id,user,product})
-    res.status(StatusCodes.OK).json({favorite,msg:'successfully deleted favorite'})
-}
-const getUserFavorites = async (req, res) => {
- const {user}=req.query
-    const favorites = await Favorite.find({ user: user}).populate('product');
-    res.status(StatusCodes.OK).json(favorites);
+const addFavorite = async (req, res) => {
+  const { user, product } = req.body;
+  if (!user || !product) {
+    throw new CustomErrors.BadRequest('Please provide both user and product');
+  }
+
+  const dbProduct = await Product.findOne({ _id: product });
+  if (!dbProduct) {
+    throw new CustomErrors.NotFound(`No product found with id: ${product}`);
+  }
+
+  const favorite = await Favorite.create({ user, product });
+  await favorite.populate('product');
   
+  res.status(StatusCodes.OK).json({ favorite, msg: 'Successfully added favorite' });
 };
 
-module.exports={addFavorite,deleteFavorite,getUserFavorites}
+const deleteFavorite = async (req, res) => {
+  const { user, product } = req.query;
+  if (!user || !product) {
+    throw new CustomErrors.BadRequest('Please provide both user and product');
+  }
+
+  const dbProduct = await Product.findOne({ _id: product });
+  if (!dbProduct) {
+    throw new CustomErrors.NotFound(`No product found with id: ${product}`);
+  }
+
+  const favorite = await Favorite.findOneAndDelete({
+    _id: req.params.id,
+    user,
+    product,
+  });
+
+  if (!favorite) {
+    throw new CustomErrors.NotFound('Favorite not found');
+  }
+
+  res.status(StatusCodes.OK).json({ favorite, msg: 'Successfully deleted favorite' });
+};
+
+const getUserFavorites = async (req, res) => {
+  const { user } = req.query;
+  const favorites = await Favorite.find({ user }).populate('product');
+  res.status(StatusCodes.OK).json(favorites);
+};
+
+module.exports = { addFavorite, deleteFavorite, getUserFavorites };
